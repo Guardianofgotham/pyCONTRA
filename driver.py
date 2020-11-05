7668a80b77d9bcc47d7e539e026617280bc794cb
Lord Hardik <hardik18391@iiitd.ac.in>
2020-11-05 17:54:20 +0530

    Unique_var_names

--- driver.py
+++ driver.py
@@ -17,7 +17,7 @@ cMatrix = sc.read(pathToCountMatrix, cache=True).T
 cMatrix.var_names = pd.read_csv(pathToGenesMap, header=None, sep='\t')[1]
 cMatrix.obs_names = pd.read_csv(pathToBarcodes, header=None, sep='\t')[0]
 
-# cMatrix.var_names_make_unique()
+cMatrix.var_names_make_unique()
 print(cMatrix)
 print(type(cMatrix))
 obj = MAST(cMatrix)
