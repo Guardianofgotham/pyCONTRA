c8f5e90db26a5e9a109ee3163755aeb4d639a780
Lord Hardik <hardik18391@iiitd.ac.in>
2020-11-05 17:57:24 +0530

    Creating Class MAST

--- pyMAST/objectFile.py
+++ pyMAST/objectFile.py
@@ -1,6 +1,9 @@
 from anndata import AnnData;
 
 class MAST:
+    """
+        Creates a MAST Object from scanpy object to work upon
+    """
     def __init__(self, sc: AnnData):
         self.data = sc.X
         
